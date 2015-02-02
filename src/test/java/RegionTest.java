bd48105970c62e127c4937eab90b5853b3a5a2d3
dobrakmato <dobrakmato@zoho.com>
2015-02-02 22:29:08 +0100

    updated pom to include junit

--- src/test/java/RegionTest.java
+++ src/test/java/RegionTest.java
@@ -1,21 +1,9 @@
-import static org.junit.Assert.*;
-
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.util.Vector;
 import org.junit.Test;
-
-import eu.matejkormuth.bukkit.Region;
-import eu.matejkormuth.bukkit.Worlds;
 
 public class RegionTest {
 
     @Test
     public void test() {
-        Region region = new Region(Vector.getRandom(), Vector.getRandom(),
-                Worlds.first());
-        for (Block b : region) {
-            b.setType(Material.STONE);
-        }
+
     }
 }
